b97ad90abb0b807f5832f08e5ef7831a2a2ae7eb
Antonio Cruz <antonio.cruz5612@gmail.com>
2023-07-11 16:47:53 -0700

    add practice code in Main() function to play with explicit type-casting.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Casting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Examples of Explicit Casting
+            double x = 24;
+            int y = 12;
+            float z = 6;
+            char t = '+';
+
+            string s = x.ToString();
+            s = z.ToString();
+            s = t.ToString();
+            s = y.ToString();
+
+            Console.WriteLine(s.GetType());   
+        }
+    }
+}
